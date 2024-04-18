60ec39c877c7d0118f62aa6881a3c4518cf8fa3a
Humberto Sandmann <hsandmann@ieee.org>
2024-04-18 09:53:40 -0300

    tratamento de excecoes

---
+++ src/main/java/espm/aulas/aula08/ErrorMain.java
@@ -0,0 +1,12 @@
+package espm.aulas.aula08;
+
+public class ErrorMain {
+
+    public static void main(String[] args) {
+
+        float[][][][] valores = new float[1000][1000][1000][1000];
+        System.out.println("tamanho do array: " + valores.length); 
+        
+    }
+
+}
---
+++ src/main/java/espm/banco/BancoException.java
@@ -0,0 +1,9 @@
+package espm.banco;
+
+public class BancoException extends RuntimeException {
+
+    public BancoException(String message) {
+        super(message);
+    }
+    
+}
